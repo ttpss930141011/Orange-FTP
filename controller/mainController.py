98c95ac823fa7393dc22cc2cbf9c4655978110f6
ttpss930141011 <o927416847@gamil.com>
2022-10-31 09:33:00 +0800

    feat : add auto upload option & customized file path

--- controller/mainController.py
+++ controller/mainController.py
@@ -7,14 +7,17 @@ from os.path import isfile, join
 from lib.ftplib import FTP,error_perm
 from pubsub import pub
 from views.UI import Main_Window
+from sys import exit
 
 class Window(QMainWindow,Main_Window):
     def __init__(self):
         super().__init__()
         self.__initVal()
+        self.__initLogMode()
         self.__initControl()
         self.__initFtp()
         self.__bindFtpMessageListen()
+        self.__autoUpload()
         
 
     def __initVal(self):
@@ -25,17 +28,23 @@ class Window(QMainWindow,Main_Window):
         _, _, ftpPath = config.readline().partition("FTP_Path:")
         _, _, ftpAccount = config.readline().partition("FTP_Account:")
         _, _, ftpPassword = config.readline().partition("FTP_Password:")
-
+        _, _, autoUpload = config.readline().partition("Auto_Upload:")
+        _, _, targetFilePath = config.readline().partition("Target_Filepath:")
         # 自動帶入setting.txt
         self.ftpHostInput.setText(ftpHost)
         self.ftpPathInput.setText(ftpPath)
         self.ftpAccountInput.setText(ftpAccount)
         self.ftpPasswordInput.setText(ftpPassword)
-        # 自動帶入file folder 內的檔案
-        for file in listdir("./file"): 
-            if isfile(join("./file", file)):
-                # self.FileList.setFileName(absresgetter.getabsres(file))
-                self.FileList.setFileName(os.path.abspath(f'file/{file}'))
+        self.autoUpload = True if autoUpload and autoUpload.strip().lower() == 'yes' else False
+        self.targetFilePath = self.__checkOutTargetFilePath(targetFilePath)
+        if self.targetFilePath:
+            # 自動帶入file folder 內的檔案 
+            for file in listdir(self.targetFilePath): 
+                if isfile(join(self.targetFilePath, file)):
+                    self.FileList.setFileName(join(self.targetFilePath, file))
+    def __initLogMode(self):
+        if self.autoUpload == True:
+            self.logFile = open("ftp.log", "a")
 
     def __initControl(self):
         self.DialogButtonBox.accepted.connect(self.StartToUpload)
@@ -55,7 +64,7 @@ class Window(QMainWindow,Main_Window):
             except:
                 self.__appendTextInFtpMsgBrower(f'*error* \'Login error\'')
                 self.ftp = NULL
-
+    
     def StartToUpload(self):
         self.__initFtp()
         self.__rstripFormData()
@@ -72,12 +81,25 @@ class Window(QMainWindow,Main_Window):
                 self.__uploadFile(localPath)
                 self.__appendTextInFtpMsgBrower(f'*uploading* \'{os.path.basename(localPath)}\'')
             self.__appendTextInFtpMsgBrower('----- upload complete -----')
-
+    
+    def __autoUpload(self):
+        if self.autoUpload == True:
+            self.StartToUpload()
+            self.__onCancelAndExit()
+            exit(0)
+            
     def __onCancelAndExit(self):
         if not self.ftp: self.ftp.close()
         self.__appendTextInFtpMsgBrower('Bye~~')
+        if not self.logFile: self.logFile.close()
         app = QApplication.instance()
         app.quit()
+
+    def __checkOutTargetFilePath(self,dir):
+        if os.path.isdir(dir):
+            return dir
+        else:
+            return
     
     def __ftpConnect(self,host,username,password):
         ftp = FTP()
@@ -102,7 +124,7 @@ class Window(QMainWindow,Main_Window):
         bufsize = 1024
         remoteFilename = os.path.basename(localpath)
         with open(localpath, 'rb') as f:
-            self.ftp.storbinary("STOR %s"%remoteFilename, f, bufsize)
+            self.ftp.storbinary(f"STOR {remoteFilename}", f, bufsize)
 
     # 監聽者function綁定
     def __bindFtpMessageListen(self):
@@ -111,9 +133,16 @@ class Window(QMainWindow,Main_Window):
     def __ftpMessageListen(self,msg):
         self.__appendTextInFtpMsgBrower(msg)
 
+    # 紀錄形式
     def __appendTextInFtpMsgBrower(self,text):
-        self.ftpMessageBrowser.append(text)
-    
+        if self.autoUpload == True:
+            self.__appendTextInLog(text)
+        else:
+            self.ftpMessageBrowser.append(text)
+
+    def __appendTextInLog(self,text):
+        self.logFile.write(f'{text}\n')
+
     def __rstripFormData(self):
         self.ftpHostInput.setText(self.ftpHostInput.text().rstrip())
         self.ftpPathInput.setText(self.ftpPathInput.text().rstrip())
